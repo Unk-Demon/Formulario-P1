1a248b923dfae2b04996c1a715b8011dbadd6c30
Unk-Demon <116051319+Unk-Demon@users.noreply.github.com>
2023-11-03 19:04:58 -0700

    Functionality finished

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById("form").addEventListener('submit', function(e){
+
+    e.preventDefault();
+
     var estudiantes = {
         nombre: document.getElementById("nombre").value,
         apellido: document.getElementById("apellido").value,
@@ -6,4 +9,28 @@ document.getElementById("form").addEventListener('submit', function(e){
         edad: document.getElementById("edad").value,
         hobbies: document.getElementById("hobbies").value
     };
+
+    addTable(estudiantes);
 });
+
+function addTable(estudiantes){
+    var tbody = document.getElementById("cuerpoTabla");
+    var fila = document.createElement("tr");
+
+    for(var i in estudiantes){
+        var td = document.createElement("td");
+        td.textContent = estudiantes[i];
+        fila.appendChild(td);
+    }
+
+    var td = document.createElement("td");
+    var btnDelete = document.createElement("button");
+    btnDelete.textContent = "Eliminar";
+    btnDelete.onclick = function(){
+        tbody.removeChild(fila).closest;
+    }
+    td.appendChild(btnDelete);
+    fila.appendChild(td);
+
+    tbody.appendChild(fila);
+}
