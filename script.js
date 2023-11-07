7c2a487164f80a7360e357b9e015ab1ad87b10dc
Unk-Demon <116051319+Unk-Demon@users.noreply.github.com>
2023-11-06 20:05:24 -0800

    movie form finished

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ document.getElementById("form").addEventListener('submit', function(e){
 
     e.preventDefault();
 
-    var estudiantes = {
-        nombre: document.getElementById("nombre").value,
-        apellido: document.getElementById("apellido").value,
-        correo: document.getElementById("correo").value,
-        edad: document.getElementById("edad").value,
-        hobbies: document.getElementById("hobbies").value
+    var peliculas = {
+        titulo: document.getElementById("titulo").value,
+        anio: document.getElementById("anio").value,
+        duracion: document.getElementById("duracion").value,
+        genero: document.getElementById("genero").value,
+        director: document.getElementById("director").value,
+        sinopsis: document.getElementById("sinopsis").value
     };
 
-    addTable(estudiantes);
+    addTable(peliculas);
+
+    e.target.reset();
 });
 
-function addTable(estudiantes){
-    var tbody = document.getElementById("cuerpoTabla");
-    var fila = document.createElement("tr");
+function addTable(peliculas){
+    const tbody = document.getElementById("cuerpoTabla");
+    const fila = document.createElement("tr");
 
-    for(var i in estudiantes){
-        var td = document.createElement("td");
-        td.textContent = estudiantes[i];
+    for(var i in peliculas){
+        const td = document.createElement("td");
+        td.textContent = peliculas[i];
         fila.appendChild(td);
     }
 
-    var td = document.createElement("td");
-    var btnDelete = document.createElement("button");
+    const td = document.createElement("td");
+    const btnDelete = document.createElement("button");
     btnDelete.textContent = "Eliminar";
     btnDelete.classList.add("btn", "btn-danger");
     btnDelete.onclick = function(){
@@ -32,6 +35,34 @@ function addTable(estudiantes){
     }
     td.appendChild(btnDelete);
     fila.appendChild(td);
+
+    const td2 = document.createElement("td");
+    const btnEdit = document.createElement("button");
+    btnEdit.textContent = "Editar";
+    btnEdit.classList.add("btn", "btn-success");
+    btnEdit.onclick = function(){
+        document.getElementById("titulo").value = fila.children[0].innerHTML;
+        document.getElementById("anio").value = fila.children[1].innerHTML;
+        document.getElementById("duracion").value = fila.children[2].innerHTML;
+        document.getElementById("genero").value = fila.children[3].innerHTML;
+        document.getElementById("director").value = fila.children[4].innerHTML;
+        document.getElementById("sinopsis").value = fila.children[5].innerHTML;
+        
+        document.getElementById("editBtn").style.display = "inline";
+        document.getElementById("addBtn").style.display = "none";
+
+        document.getElementById("editBtn").onclick = function(){
+            fila.children[0].innerHTML = document.getElementById("titulo").value;
+            fila.children[1].innerHTML = document.getElementById("anio").value;
+            fila.children[2].innerHTML = document.getElementById("duracion").value;
+            fila.children[3].innerHTML = document.getElementById("genero").value;
+            fila.children[4].innerHTML = document.getElementById("director").value;
+            fila.children[5].innerHTML = document.getElementById("sinopsis").value;
+        }
+    }
+
+    td2.appendChild(btnEdit);
+    fila.appendChild(td2);
 
     tbody.appendChild(fila);
 }
